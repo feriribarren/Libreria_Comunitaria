616aac39472c9d2a96ff5bdf1cc64bdab79d0bf4
FerIribarren <64477772+feriribarren@users.noreply.github.com>
2020-05-22 12:05:40 -0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -4,21 +4,47 @@ const ejemplosLibros=[
     autor:"Gabriel Garcia Marquez",
     anioLanzamiento:1993,
     editorial:"San Sebastian",
-    isbn: "1234593827364",  
+    isbn: "1234593827364",
+    alquileres:[
+      {
+        fecha: "20/05/2020",
+        devuelto: false,
+      },
+    ]  
   },
   {
     titulo:"El coronel no tiene quien le escriba",
     autor:"Gabriel Garcia Marquez",
     anioLanzamiento:1989,
     editorial:"Galaxias",
-    isbn: "1234583725361",  
+    isbn: "1234583725361",
+    alquileres:[
+      {
+        fecha: "20/04/2020",
+        devuelto: true,
+      },
+      {
+        fecha: "21/05/2020",
+        devuelto: false,
+      },
+    ]
   },
   {
     titulo:"La isla bajo el mar",
     autor:"Isabel Allende",
     anioLanzamiento:1980,
     editorial:"Chile ltda",
-    isbn: "1234772261251",  
+    isbn: "1234772261251",
+    alquileres:[
+      {
+        fecha: "20/04/2020",
+        devuelto: true,
+      },
+      {
+        fecha: "17/03/2020",
+        devuelto: true,
+      },
+    ]
   },
   {
     titulo:"En Cambio",
@@ -26,5 +52,15 @@ const ejemplosLibros=[
     anioLanzamiento:2010,
     editorial:"Caja de libros",
     isbn: "1234592030095",  
+    alquileres:[
+      {
+        fecha: "10/01/2020",
+        devuelto: true,
+      },
+      {
+        fecha: "10/05/2020",
+        devuelto: false,
+      },
+    ]
   },
 ]
