d895cff3b029d64fdbf810f4471b163377ea11bd
FerIribarren <64477772+feriribarren@users.noreply.github.com>
2020-05-22 12:00:02 -0300

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,30 @@
+const ejemplosLibros=[
+  {
+    titulo:"El amor en tiempos de colera",
+    autor:"Gabriel Garcia Marquez",
+    anioLanzamiento:1993,
+    editorial:"San Sebastian",
+    isbn: "1234593827364",  
+  },
+  {
+    titulo:"El coronel no tiene quien le escriba",
+    autor:"Gabriel Garcia Marquez",
+    anioLanzamiento:1989,
+    editorial:"Galaxias",
+    isbn: "1234583725361",  
+  },
+  {
+    titulo:"La isla bajo el mar",
+    autor:"Isabel Allende",
+    anioLanzamiento:1980,
+    editorial:"Chile ltda",
+    isbn: "1234772261251",  
+  },
+  {
+    titulo:"En Cambio",
+    autor:"Estanislao Bachrach",
+    anioLanzamiento:2010,
+    editorial:"Caja de libros",
+    isbn: "1234592030095",  
+  },
+]
